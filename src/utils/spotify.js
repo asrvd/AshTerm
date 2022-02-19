c13b9f710bb97c359c7f460389bc1215c9e73343
ashish <68690233+asheeeshh@users.noreply.github.com>
2022-02-19 16:31:09 +0530

    Update spotify.js

--- src/utils/spotify.js
+++ src/utils/spotify.js
@@ -2,5 +2,5 @@ export default async function getnp() {
     const res = await fetch('https://spotify-np-api.vercel.app/api', {mode: 'cors'});
     console.log(res)
     const data = await res.json();
-    return data;
+    return data['np'];
 }
